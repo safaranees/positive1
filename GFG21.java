97027126186468855b4bdf7fb7526e1e5bc9f466
safaranees <aneesparvin08@gmail.com>
2018-09-13 21:01:10 +0530

    Create GFG21.java

---
+++ GFG21.java
@@ -0,0 +1,26 @@
+import java.io*;
+import java.util.*;
+class GFG{
+     
+    // Function to find sum of series.
+    static float sumOfAP(float a, float d, 
+                                  int n)
+    {
+        float sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + a;
+            a = a + d;
+        }
+        return sum;
+    }
+     
+    // Driver function
+    public static void main(String args[])
+    {
+        int n = 20;
+        float a = 2.5f, d = 1.5f;
+        System.out.println(sumOfAP(a, d, n));
+    }
+}
+ 
