c78960ac8d330cd4150e17c051d416de45c60f2a
safaranees <aneesparvin08@gmail.com>
2018-09-13 20:14:34 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,20 @@
+import java.io.*;
+ 
+class GFG {
+
+static int findSum(int N, int K)
+{
+int ans = 0;
+for (int i = 1; i <= N; i++)
+ans += (i % K);
+return ans;
+}
+static public void main(String[] args)
+{
+int N = 10, K = 2;
+System.out.println(findSum(N, K));
+}
+}
+ 
+
+
