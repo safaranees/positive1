00d309b35a9a6edb948f666030d366143f1d00b1
safaranees <aneesparvin08@gmail.com>
2019-03-18 14:59:21 +0530

    Update amstrong1.java

--- amstrong1.java
+++ amstrong1.java
@@ -1,35 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-public class Armstrong {
+public class Armstrong1 {
 
     public static void main(String[] args) {
-
-        int low = 999, high = 99999;
-
-        for(int number = low + 1; number < high; ++number) {
-            int digits = 0;
-            int result = 0;
-            int originalNumber = number;
-
-            // number of digits calculation
-            while (originalNumber != 0) {
-                originalNumber /= 10;
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        for(int i=a+1;i<b;i++)
+        {
+            int digits=0;
+            int result=0;
+            int num=i;
+            while(num!=0)
+            {
+                num=num/10;
                 ++digits;
             }
-
-            originalNumber = number;
-
-            // result contains sum of nth power of its digits
-            while (originalNumber != 0) {
-                int remainder = originalNumber % 10;
-                result += Math.pow(remainder, digits);
-                originalNumber /= 10;
+            num=i;
+            while(num!=0)
+            {
+                int rem=num%10;
+                result+=Math.pow(rem,digits);
+                num=num/10;
             }
-
-            if (result == number)
-                System.out.print(number + " ");
+            if(result==i)
+            {
+                System.out.println(i+" ");
+            }
         }
     }
 }
 
+       
