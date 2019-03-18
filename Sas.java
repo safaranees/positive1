5fe86a35aad029d0820c9daf45b5c1d225d87570
safaranees <aneesparvin08@gmail.com>
2019-03-18 15:59:35 +0530

    Update Sas.java

--- Sas.java
+++ Sas.java
@@ -1,16 +1,33 @@
 import java.io.*;
+
 import java.util.*;
+
 class Sas
+
 {
-	public static void main(String args[])
-	{
-		Scanner ss=new Scanner(System.in);
-		int n=ss.nextInt();
-		int  a=ss.nextInt();
-		int d=ss.nextInt();
-		int c=n-1;
-		int s=(n/2)*(2*a+c*d);
-		System.out.println(s);
-	}
+	
+public static void main(String args[])
+	
+{
+		
+Scanner ss=new Scanner(System.in);
+		
+float n=ss.nextInt();
+		
+float  a=ss.nextInt();
+		
+int d=ss.nextInt();
+		
+
+float b=n/2;
+float c=2*a;
+float e=n-1;
+float f=e*d;
+float h=c+f;
+float s=b*h;		
+System.out.println(s);
+	
+}
+
 }
 
