40210e56dbf096c67e267f60be7180ba853f0123
safaranees <aneesparvin08@gmail.com>
2019-03-18 12:15:52 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,23 +1,28 @@
+
 import java.io.*;
 import java.util.*;
-
-public class Palindrome {
-
-    public static void main(String[] args) {
-
-intnum = 121, reversedInteger = 0, remainder, originalInteger;
-
-originalInteger = num; 
-while( num != 0 )
+class Palindrome
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int rev=0,rem;
+        int num=n;
+        while(n!=0)
         {
-            remainder = num % 10;
-reversedInteger = reversedInteger * 10 + remainder;
-num  /= 10;
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
         }
-        if (originalInteger == reversedInteger)
-System.out.println(originalInteger + " is a palindrome.");
+        if(rev==num)
+        {
+            System.out.println("yes");
+        }
         else
-System.out.println(originalInteger + " is not a palindrome.");
+        {
+            System.out.println("no");
+        }
     }
 }
 
