54d14bd015e2c4655ffd4653881214c911f135d1
safaranees <aneesparvin08@gmail.com>
2019-03-18 12:44:32 +0530

    Update and rename HWSProblem20.java to Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Multiples
+{
+   public static void main(String args[])
+   {
+      Scanner s=new Scanner(System.in);
+      int n=s.nextInt();
+      int m;
+      for(int i=1;i<=n;i++)
+      {
+         m=i*n;
+         System.out.println(m);
+      }
+   }
+}
