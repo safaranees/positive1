10d7d9658afb38ec85cd390b4395bc5fb11a1e4e
safaranees <aneesparvin08@gmail.com>
2019-03-18 16:29:07 +0530

    Update Maximum.java

--- Maximum.java
+++ Maximum.java
@@ -1,30 +1,27 @@
 import java.io.*;
-import java.util.*;    
+import java.util.*;
 class Maximum
 {
-	public static void main(String args[])
+ 	public static void main(String args[])
 	{
 		Scanner s=new Scanner(System.in);
 		int[] arr=new int[50];
-		int size=s.nextInt();
 		int max;
+		int size=s.nextInt();
 		for(int i=0;i<size;i++)
 		{
-			arr[i]=s.nextInt();
+   			arr[i]=s.nextInt();
 		}
-		max=arr[size-1];
-       		for(int i=1;i<size;i++)
-        		{
-			if(arr[i]>max)
-			{
-				max=arr[i];
-				
+		max=arr[0];
+ 		for(int i=1;i<size;i++)
+    		{
+    			if(arr[i]>max)
+     			{
+    				max=arr[i];
 			}
-       		}     
+     		 }
 		System.out.println(max);
-       	}
+	}
 }
-
-
-
-
+     
+     
