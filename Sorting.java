891bb7c89bee5197aca209b11dcf2db3a29df995
safaranees <aneesparvin08@gmail.com>
2019-05-13 20:13:49 +0530

    Update and rename GFG24.java to Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class Sorting
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] arr=new int[100];
+		int temp;
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = i + 1; j < n; j++) 
+			{
+				if (arr[i] > arr[j]) 
+				{
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < n ; i++) 
+		{
+			System.out.println(arr[i]);
+		}
+		
+	}
+}
