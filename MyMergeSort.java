a4952150dc30a108d0764203febc7b25ca3c47f4
safaranees <aneesparvin08@gmail.com>
2019-05-14 14:33:47 +0530

    Update and rename quicksort.java to MyMergeSort.java

---
+++ MyMergeSort.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+public class MyMergeSort  
+{  
+void merge(int arr[], int beg, int mid, int end)  
+{  
+  
+int l = mid - beg + 1;  
+int r = end - mid;  
+  
+int LeftArray[] = new int [l];  
+int RightArray[] = new int [r];  
+  
+for (int i=0; i<l; ++i)  
+LeftArray[i] = arr[beg + i];  
+  
+for (int j=0; j<r; ++j)  
+RightArray[j] = arr[mid + 1+ j];  
+  
+  
+int i = 0, j = 0;  
+int k = beg;  
+while (i<l&&j<r)  
+{  
+if (LeftArray[i] <= RightArray[j])  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+}  
+else  
+{  
+arr[k] = RightArray[j];  
+j++;  
+}  
+k++;  
+}  
+while (i<l)  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+k++;  
+}  
+  
+while (j<r)  
+{  
+arr[k] = RightArray[j];  
+j++;  
+k++;  
+}  
+}  
+  
+void sort(int arr[], int beg, int end)  
+{  
+if (beg<end)  
+{  
+int mid = (beg+end)/2;  
+sort(arr, beg, mid);  
+sort(arr , mid+1, end);  
+merge(arr, beg, mid, end);  
+}  
+}
+
+public static void main(String args[])
+{
+	Scanner s=new Scanner (System.in);
+	int arr[]=new int[10];
+	int n=s.nextInt();
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=s.nextInt();
+	}
+	MyMergeSort ob=new MyMergeSort();
+	ob.sort(arr,0,arr.length-1);
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println(arr[i]+" ");
+	}
+}
+}
+
+
+  
+
